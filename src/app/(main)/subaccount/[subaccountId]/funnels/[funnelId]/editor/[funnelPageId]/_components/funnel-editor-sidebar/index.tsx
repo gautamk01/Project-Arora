abd2ea26658273047a7f0478636dfee8b01eefe2
gautamk01 <charmingdeveloper@protonmail.com>
2024-04-03 23:31:02 +0530

    Added the Setting tabs content then Created a component for the setting tabs

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/index.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/index.tsx
@@ -1,11 +1,18 @@
 "use client";
-import { Sheet, SheetContent } from "@/components/ui/sheet";
-import { Tabs } from "@/components/ui/tabs";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
 import { useEditor } from "@/Provider/editor/editor-provider";
 import clsx from "clsx";
 //here we are going to build a skaliton for the sidebar
 import React from "react";
 import TabList from "./tabs";
+import SettingTabs from "./tabs/settins-tab";
 
 type Props = {
   subaccountId: string;
@@ -16,6 +23,7 @@ const FunnelEditorSidebar = ({ subaccountId }: Props) => {
   return (
     <Sheet open={true} modal={false}>
       <Tabs className="w-full" defaultValue="Settings">
+        {/* Sheet: List the Tabs */}
         <SheetContent
           showX={false}
           side="right"
@@ -25,6 +33,29 @@ const FunnelEditorSidebar = ({ subaccountId }: Props) => {
           )}
         >
           <TabList />
+        </SheetContent>
+
+        {/* Sheet: This is for render the contents for secific tabs */}
+        <SheetContent
+          showX={false}
+          side="right"
+          className={clsx(
+            "mt-[97px] w-80 z-[40] shadow-none p-0 mr-16 bg-background h-full transition-all overflow-hidden",
+            { hidden: state.editor.previewMode }
+          )}
+        >
+          <div className=" grid gap-4 h-full pb-36 overflow-scroll">
+            <TabsContent value="Settings">
+              <SheetHeader className=" text-left p-6">
+                <SheetTitle>Style</SheetTitle>
+                <SheetDescription>
+                  Show your Creativity! you can customize every components as
+                  you like
+                </SheetDescription>
+              </SheetHeader>
+              <SettingTabs />
+            </TabsContent>
+          </div>
         </SheetContent>
       </Tabs>
     </Sheet>
---
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/tabs/settins-tab.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SettingTabs = (props: Props) => {
+  return <div>SettingTabs</div>;
+};
+
+export default SettingTabs;
